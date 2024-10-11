fc6a8347a73cb8df4054606d3c6db8306c6fb638
Rennan Barros <rennan_sbarros@hotmail.com>
2024-10-11 00:56:05 -0300

    test: instalando e configurando Jest e React Testing Library

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+export {};
+
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'], 
+    moduleNameMapper: {
+      '\\.(css|scss)$': 'identity-obj-proxy',
+    },
+  };
